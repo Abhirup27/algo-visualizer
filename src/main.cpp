2d275d3054612432ba1e3a75e5345e78533bb6ff
Abhirup27 <abhirup27022001@outlook.com>
2025-08-20 19:32:30 +0530

    modified cmake to add WASM support

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,101 @@
 #include "raylib.h"
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#endif
+
 #include <iostream>
 
 #pragma region imgui
 #include "imgui.h"
-#include "rlImGui.h"
 #include "imguiThemes.h"
+#include "rlImGui.h"
+ImGuiIO *g_io = nullptr;
 #pragma endregion
 
+void UpdateDrawFrame(void);
+int main(void) {
 
-
-int main(void)
-{
-
-	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-	InitWindow(800, 450, "raylib [core] example - basic window");
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  InitWindow(800, 450, "raylib [core] example - basic window");
 
 #pragma region imgui
-	rlImGuiSetup(true);
+  rlImGuiSetup(true);
 
-	//you can use whatever imgui theme you like!
-	//ImGui::StyleColorsDark();
-	//imguiThemes::yellow();
-	//imguiThemes::gray();
-	imguiThemes::green();
-	//imguiThemes::red();
-	//imguiThemes::embraceTheDarkness();
+  // you can use whatever imgui theme you like!
+  // ImGui::StyleColorsDark();
+  // imguiThemes::yellow();
+  // imguiThemes::gray();
+  imguiThemes::green();
+  // imguiThemes::red();
+  // imguiThemes::embraceTheDarkness();
 
+  g_io = &ImGui::GetIO();
+  // ImGuiIO &io = ImGui::GetIO(); (void)io;
+  g_io->ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  g_io->ConfigFlags |= ImGuiConfigFlags_DockingEnable; // Enable Docking
+  g_io->FontGlobalScale = 2;
 
-	ImGuiIO &io = ImGui::GetIO(); (void)io;
-	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
-	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
-	io.FontGlobalScale = 2;
-
-	ImGuiStyle &style = ImGui::GetStyle();
-	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-	{
-		//style.WindowRounding = 0.0f;
-		style.Colors[ImGuiCol_WindowBg].w = 0.5f;
-		//style.Colors[ImGuiCol_DockingEmptyBg].w = 0.f;
-	}
+  ImGuiStyle &style = ImGui::GetStyle();
+  if (g_io->ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    // style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 0.5f;
+    // style.Colors[ImGuiCol_DockingEmptyBg].w = 0.f;
+  }
 
 #pragma endregion
+#if defined(PLATFORM_WEB)
+  emscripten_set_main_loop(UpdateDrawFrame, 0, 1);
+#else
+  SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+  //--------------------------------------------------------------------------------------
 
-
-
-	while (!WindowShouldClose())
-	{
-		BeginDrawing();
-		ClearBackground(RAYWHITE);
-
-
-	#pragma region imgui
-		rlImGuiBegin();
-
-		ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
-		ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
-		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
-		ImGui::PopStyleColor(2);
-	#pragma endregion
-
-
-		ImGui::Begin("Test");
-
-		ImGui::Text("Hello");
-		ImGui::Button("Button");
-		ImGui::Button("Button2");
-
-		ImGui::End();
-
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-
-	#pragma region imgui
-		rlImGuiEnd();
-
-		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
-		{
-			ImGui::UpdatePlatformWindows();
-			ImGui::RenderPlatformWindowsDefault();
-		}
-	#pragma endregion
-
-		EndDrawing();
-	}
-
+  // Main game loop
+  while (!WindowShouldClose()) // Detect window close button or ESC key
+  {
+    UpdateDrawFrame();
+  }
+#endif
 
 #pragma region imgui
-	rlImGuiShutdown();
+  rlImGuiShutdown();
 #pragma endregion
 
+  CloseWindow();
 
+  return 0;
+}
 
-	CloseWindow();
+void UpdateDrawFrame(void) {
+  BeginDrawing();
+  ClearBackground(RAYWHITE);
 
-	return 0;
+#pragma region imgui
+  rlImGuiBegin();
+
+  ImGui::PushStyleColor(ImGuiCol_WindowBg, {});
+  ImGui::PushStyleColor(ImGuiCol_DockingEmptyBg, {});
+  ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+  ImGui::PopStyleColor(2);
+#pragma endregion
+
+  ImGui::Begin("Test");
+
+  ImGui::Text("Hello");
+  ImGui::Button("Button");
+  ImGui::Button("Button2");
+
+  ImGui::End();
+
+  DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+#pragma region imgui
+  rlImGuiEnd();
+
+  if (g_io->ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    ImGui::UpdatePlatformWindows();
+    ImGui::RenderPlatformWindowsDefault();
+  }
+#pragma endregion
+  EndDrawing();
 }
